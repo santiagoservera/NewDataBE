ef017ec76fcbc24433e23e3a1dd30902e819012f
Santiagoservera <santiagoservera21@gmail.com>
2024-11-25 22:12:25 -0300

    conexion bd

--- src/db.js
+++ src/db.js
@@ -1,8 +1,15 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv"; // Importar dotenv para cargar las variables de entorno
+
+dotenv.config(); // Cargar las variables de entorno
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/mern-crud-auth");
+    // Usar la URI de conexión de MongoDB Atlas desde el archivo .env
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Conectado a la base de datos de MongoDB!");
   } catch (error) {
     console.error("Error al conectar a la base de datos de MongoDB:", error);
