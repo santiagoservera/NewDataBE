69c3f316962d411a7a476be340167a1ae1aff2ec
Santiagoservera <santiagoservera21@gmail.com>
2024-11-26 12:02:28 -0300

    configuracion de puerto

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import app from "./app.js";
 import { connectDB } from "./db.js";
+const port = process.env.PORT || 4000;
 
 connectDB();
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(port, () => {
+  console.log(`Server is running on port ${port} `);
 });
